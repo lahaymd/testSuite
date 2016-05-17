c65c3c34a0a8c4e99593c9b7455a2c5282e0fdff
lahaymd <lahaymd@gmail.com>
2016-05-17 12:11:16 -0500

    changed comment

--- tdd/lib/my-app.js
+++ tdd/lib/my-app.js
@@ -7,7 +7,7 @@ var _ = require('underscore.string');
 var port = 8080;
 
 
-//random comment
+//random comment 2
 
 var start= exports.start = function start(port, cb) {
 	server= app.listen(port, cb)
