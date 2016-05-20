18acc8041545d6a62f05fef64b15ba8a23a7be4f
lahaymd <lahaymd@gmail.com>
2016-05-20 08:50:04 -0500

    i told the truth today

--- tdd/lib/my-app.js
+++ tdd/lib/my-app.js
@@ -7,7 +7,7 @@ var _ = require('underscore.string');
 var port = 8080;
 
 
-//random comment 12345678 hotfixed
+//random comment 12345678 hotfixed boss
 
 var start= exports.start = function start(port, cb) {
 	server= app.listen(port, cb)
